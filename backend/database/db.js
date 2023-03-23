c7a571df6543981c7c711b562d683dd6d6bcaae0
Neetu Kumari <chaudharyneetu749@gmail.com>
2023-03-23 13:44:41 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const Connection=async(username,password)=>{
 
-    const URL="mongodb+srv://ghoon:ghoon@cluster0.6e7csob.mongodb.net/crud_api?retryWrites=true&w=majority";
+    const URL="mongodb+srv://<DB_USERNAME>:<DB_PASSWORD>@cluster0.6e7csob.mongodb.net/crud_api?retryWrites=true&w=majority";
 
     try{
         await mongoose.connect(URL,{useUnifiedTopology:true,useNewUrlParser:true});
